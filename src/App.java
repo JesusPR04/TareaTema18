17826abf25dcd83b9158961c5fe7bbf1fdef4ff5
JesusPR04 <jesuspastorrobles12@gmail.com>
2023-04-16 11:41:46 +0200

    Segundo cambio develop

--- src/App.java
+++ src/App.java
@@ -2,5 +2,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         // Version 0.1 en la rama master.
         // Primera modificación en la rama develop.
+        // Segunda modificación en la rama develop.
     }
 }
