2b059aa1471be9699b84c792a9a876cce44eb5a3
JesusPR04 <jesuspastorrobles12@gmail.com>
2023-04-16 11:32:51 +0200

    Cambio 1 feature 1

--- src/App.java
+++ src/App.java
@@ -2,5 +2,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         // Version 0.1 en la rama master.
         // Primera modificación en la rama develop.
+        // Cambio 1 de la rama feature 1.
     }
 }
