7515a42e411c3414bfe2d5b4551d07864a898739
JesusPR04 <jesuspastorrobles12@gmail.com>
2023-04-16 11:56:22 +0200

    Tercer cambio develop

--- src/App.java
+++ src/App.java
@@ -3,5 +3,6 @@ public class App {
         // Version 0.1 en la rama master.
         // Primera modificación en la rama develop.
         // Segunda modificación en la rama develop.
+        // Tercera modificación en la rama develop.
     }
 }
