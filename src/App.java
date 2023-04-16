9ba1f9f4600642e6a727948ea0763445cd478407
JesusPR04 <jesuspastorrobles12@gmail.com>
2023-04-16 11:39:53 +0200

    Master v0.2

--- src/App.java
+++ src/App.java
@@ -2,5 +2,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         // Version 0.1 en la rama master.
         // Cambio en la rama hotfix.
+        // Version 0.2 en la rama master.
     }
 }
