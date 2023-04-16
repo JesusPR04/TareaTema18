0ca0efdcb471380d2595646522f339fc918e9db6
JesusPR04 <jesuspastorrobles12@gmail.com>
2023-04-16 11:29:53 +0200

    Primer cambio develop

--- src/App.java
+++ src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
         // Version 0.1 en la rama master.
+        // Primera modificación en la rama develop.
     }
 }
